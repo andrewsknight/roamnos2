e1a9330f65636c83dbab5e356171e8061882b7c5
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2022-02-07 22:20:36 +0100

    romano_a_arabigo con diccionario muchas claves'

--- romanos.py
+++ romanos.py
@@ -1,22 +1,19 @@
 valores_romanos = {
     1: 'I',
+    4: 'IV',
     5: 'V',
+    9: 'IX',
     10: 'X',
+    40: 'XL',
     50: 'L',
+    90: 'XC',
     100: 'C',
+    400: 'CD',
     500: 'D',
+    900: 'CM',
     1000: 'M'
 }
 
-valores_romanosT = [
-    (1000, 'M'),
-    (500, 'D'),
-    (100, 'C'),
-    (50, 'L'),
-    (10, 'X'),
-    (5, 'V'),
-    (1, 'I'),
-]
 
 # 36 -> XXXVI
 def arabigo_a_romano(n):
@@ -24,17 +21,13 @@ def arabigo_a_romano(n):
     resto = None
 
     while resto != 0:
-        for valor, simbolo in valores_romanosT:
+        for valor in sorted(valores_romanos.keys(), reverse=True):
             if n >= valor:
                 break
         
         cociente = n // valor
         resto = n % valor
-        if cociente > 3:
-            romano +='*'
-
-        else:
-            romano += cociente * simbolo
-            n = resto
+        romano += cociente * valores_romanos[valor]
+        n = resto
     
     return romano
--- test_romanos.py
+++ test_romanos.py
@@ -1,3 +1,4 @@
 from romanos import arabigo_a_romano
 
 print(arabigo_a_romano(36)) # 'XXXVI'
+print(arabigo_a_romano(46)) # 'XXXVI'
